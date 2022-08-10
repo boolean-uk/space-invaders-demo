762b7e7de9b0df1551d7110fef6d3bd703731859
dearshrewdwit <dearshrewdwit@gmail.com>
2022-08-10 09:33:57 +0100

    aliens can be hit

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const state = {
   numCells: (600 / 40) * (600 / 40),
   cells: [],
-  shipPosition: 217
+  shipPosition: 217,
+  alienPositions: [
+    3, 4, 5, 6, 7, 8, 9, 10,11,
+    18,19,20,21,22,23,24,25,26,
+    33,34,35,36,37,38,39,40,41,
+    48,49,50,51,52,53,54,55,56
+  ]
 }
 
 const setupGame = (element) => {
@@ -12,6 +18,7 @@ const setupGame = (element) => {
   // draw the spaceship
   drawShip()
   // draw the aliens
+  drawAliens()
   // draw the scoreboard
 }
 
@@ -78,15 +85,85 @@ const fire = () => {
       return
     }
 
+    // if there's an alien, BOOM!
+    // clear interval, remove alien image, remove alien from positions, set a timeout for a boom emoji
+    if (state.alienPositions.includes(laserPosition)) {
+      clearInterval(interval)
+      state.alienPositions.splice(state.alienPositions.indexOf(laserPosition), 1)
+      state.cells[laserPosition].classList.remove('alien')
+      state.cells[laserPosition].classList.add('hit')
+      setTimeout(() => {
+        state.cells[laserPosition].classList.remove('hit')
+      }, 200)
+      return
+    }
+
     // add image
     state.cells[laserPosition].classList.add('laser')
   }, 100)
 }
 
+const drawAliens = () => {
+  // loop through cells, remove, and add class name to corresponding cell.
+  state.cells.forEach((cell, index) => {
+    // reset: if cell index is currently an alien position remove it
+    if (cell.classList.contains('alien')) {
+      cell.classList.remove('alien')
+    }
+    // update: if cell index is an alien position, add alien class
+    if (state.alienPositions.includes(index)) {
+      cell.classList.add('alien')
+    }
+  })
+}
+
 const play = () => {
+  // start the aliens moving!
+  let interval
+  // set starting direction
+  let direction = 'right'
+  // set interval to repeat updating alien positions and drawing them
+  interval = setInterval(() => {
+    let movement
+    // if right
+    if (direction === 'right') {
+      if (atSide('right')) {
+        // go down a row and reverse direction to the left
+        movement = 15 - 1
+        direction = 'left'
+      } else {
+        // continue right
+        movement = 1
+      }
+      // if left
+    } else if (direction === 'left') {
+      if (atSide('left')) {
+        // go down a row and reverse direction to the right
+        movement = 15 + 1
+        direction = 'right'
+      } else {
+        // continue left
+        movement = -1
+      }
+    }
+    //update alien positions
+    state.alienPositions = state.alienPositions.map(position => position + movement)
+    // redraw aliens
+    drawAliens()
+  }, 300)
   // start the ability to move and fire
   window.addEventListener('keydown', controlShip)
-  // start the aliens moving!
+
+}
+
+const atSide = (side) => {
+  if (side === 'left') {
+    // are there any aliens with a position in left hand column? (index multiple of 15)
+    return state.alienPositions.some(position => position % 15 === 0)
+  } else if (side === 'right') {
+    // are there any aliens with a position in right hand column? (index multiple of 15 -1)
+    return state.alienPositions.some(position => position % 15 === 14)
+  }
 }
 
 

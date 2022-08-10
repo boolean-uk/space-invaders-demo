207670226bca8e4dd4bb996f7d927749d71ee926
dearshrewdwit <dearshrewdwit@gmail.com>
2022-08-10 08:32:41 +0100

    1. Outline: shape of the program

---
+++ index.js
@@ -0,0 +1,23 @@
+// define the state and behaviour needed.
+const state = {
+
+}
+
+const setupGame = (element) => {
+  // draw the board
+  // draw the spaceship
+  // draw the aliens
+  // draw the scoreboard
+}
+
+const play = () => {
+  // start the aliens moving!
+}
+
+
+// query the page for the element
+const appElement = document.querySelector('.app')
+// insert app into the game
+setupGame(appElement)
+// play!
+play()
